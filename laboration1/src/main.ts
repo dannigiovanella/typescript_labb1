c5b639dffb867c9a2b9f0a1adca4d582a5c456c3
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-04-05 15:17:31 +0200

    Har lagt in händelselyssnare på formuläret som samlar in data från inputfält som hamnar i ett objekt

--- laboration1/src/main.ts
+++ laboration1/src/main.ts
@@ -1,10 +1,51 @@
 
+//Interface
 
 interface CourseInfo {
-  code: string;
-  name: string;
-  progression: "A" | "B" | "C";
-  syllabus: string;
+    code: string;
+    name: string;
+    progression: "A" | "B" | "C"; //literal type
+    syllabus: string;
 }
 
+//Formulär
 
+//Hämtar form-elementet. ! för att hantera ett eventuellt null
+const form = document.querySelector<HTMLFormElement>("#courseform")!;
+
+//Händelsyssnare för submitknapp
+form.addEventListener("submit", (event: SubmitEvent) => {
+    event.preventDefault();
+
+    //Hämtar input och select-element
+    const code = document.querySelector<HTMLInputElement>("#code")!.value;
+    const name = document.querySelector<HTMLInputElement>("#name")!.value;
+
+    //Progressionsvärde kontrolleras med if-sats
+    const progressionValue = document.querySelector<HTMLSelectElement>("#progression")!.value
+
+    //Kontroll (narrowing) för värdet på select-element av progression
+    if (
+        progressionValue !== "A" &&
+        progressionValue !== "B" &&
+        progressionValue !== "C"
+    ) {
+        //Om nåt blir fel
+        throw new Error("Felaktigt progressionsvärde");
+    }
+    const progression: "A" | "B" | "C" = progressionValue;
+
+    const syllabus = document.querySelector<HTMLInputElement>("#syllabus")!.value;
+
+
+    //Objekt med insamlad data från form
+    const newCourse: CourseInfo = {
+        code,
+        name,
+        progression,
+        syllabus
+    };
+
+
+    console.log(newCourse);
+});
